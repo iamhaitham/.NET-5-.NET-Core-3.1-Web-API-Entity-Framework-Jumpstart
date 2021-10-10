1dab2e20a88ee5169b2085d688e3b90c037dbd23
Haitham Estetieh <iamhaitham2011@gmail.com>
2021-10-10 13:13:44 +0300

    Data Seeding - Add Skills to the Database

--- Section 5 - Advanced Relationships with Entity Framework Core - .NET 5/Data/DataContext.cs
+++ Section 5 - Advanced Relationships with Entity Framework Core - .NET 5/Data/DataContext.cs
@@ -18,5 +18,13 @@ namespace Section_1___Introduction___.NET_5.Data
         public DbSet<User> Users { set; get; }
         public DbSet<Weapon> Weapons { set; get; }
         public DbSet<Skill> Skills { set; get; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Skill>().HasData(
+                new Skill() { Id = 1, Name = "Fireball", Damage = 30 },
+                new Skill() { Id = 2, Name = "Frenzy", Damage = 20 },
+                new Skill() { Id = 3, Name = "Blizzard", Damage = 50 }
+            );
+        }
     }
 }
